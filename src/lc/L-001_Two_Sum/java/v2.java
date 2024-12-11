cc20342acd2be45851bfa472ca868f52e411de51
Andrii Pyrih <andrii.pyrih@gmail.com>
2024-12-11 13:07:10 +0000

    (practice): 001_java_v2

---
+++ src/lc/L-001_Two_Sum/java/v2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> occurred = new HashMap<>();
+
+        for (int current = 0; current < nums.length; current++) {
+            int needed = target - nums[current];
+
+            if (occurred.containsKey(needed)) {
+                return new int[] {current, occurred.get(needed)};
+            }
+
+            occurred.put(nums[current], current);
+
+        }
+
+        return new int[2];
+    }
+}
